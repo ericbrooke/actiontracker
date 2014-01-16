09a8e0098864f25a55c4a2f0dc5c3504a7415e9b
Eric Brooke <ericbrooke@mac.com>
2014-01-16 13:59:41 -0800

    Added title functionality

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -7,9 +7,17 @@ feature 'Creating Projects' do
 		click_link 'New Project'
 
 		fill_in 'Name', with: 'actiontracker'
-		fill_in 'Description', with: 'an app to trcak your projects'
+		fill_in 'Description', with: 'an app to track your projects'
 		click_button 'Create Project'
 
 		expect(page).to have_content('Project has been created.')
+	
+		project = Project.where(name: "actiontracker").first
+
+		expect(page.current_url).to eql(project_url(project))
+
+		title = "actiontracker - Projects - actiontracker"
+		expect(page).to have_title(title)
 	end
+
 end
