b7b793a59a3de96c5b2797e2fd19ce234e695e76
Eric Brooke <ericbrooke@mac.com>
2014-01-17 13:57:55 -0800

    if a project does not exist the user gets redirected to the projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
   
   def index
     @projects =Project.all
@@ -21,16 +23,15 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
+  #note before action
   end
 
   def edit
-    @project = Project.find(params[:id])
+  #note before action  
   end
 
   def update
-    @project = Project.find(params[:id])
-
+  #note before action
     if @project.update(project_params)
 
       flash[:notice] = "Project has been updated."
@@ -42,11 +43,9 @@ class ProjectsController < ApplicationController
    end
 
   def destroy
-    @project =Project.find(params[:id])
-
+  #note before action
     @project.destroy
     flash[:notice] = "Project has been destroyed."
-
     redirect_to projects_path
   end
 
@@ -57,5 +56,12 @@ private
   def project_params
     params.require(:project).permit(:name, :description)
   end
+
+ def set_project
+   @project = Project.find(params[:id])
+ rescue ActiveRecord::RecordNotFound
+  flash[:alert] = "The project you were looking for could not be found."
+  redirect_to projects_path
+ end
 
 end
