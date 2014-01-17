11e668f36212b0719a5195d2d4f5b57f585af9bb
Eric Brooke <ericbrooke@mac.com>
2014-01-17 12:34:40 -0800

    You can now update a project.

---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature "Editing Projects" do
+	
+	before do
+	  FactoryGirl.create(:project, name: "Chicken")
+		visit "/"
+		click_link "Chicken"
+		click_link "Edit Project"
+	end
+
+
+	scenario "Updating project" do
+		fill_in "Name", with: "Chickens are go"
+
+		click_button "Update Project"
+
+		expect(page).to have_content("Project has been updated.")
+	end
+
+	scenario "Updating a project with no name" do
+	  fill_in "Name", with: ""
+	  click_button "Update Project"
+	  expect(page).to have_content("Project has not been updated.")
+	end
+
+end
