364a44c25b12931e80d94218139e9d9ee8e0daae
Eric Brooke <ericbrooke@mac.com>
2014-02-25 22:53:00 -0800

    setup test for permissions, view projects work but need to update all other tests and controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
 
 before_action :authorize_admin!, except: [:index, :show]
+before_action :require_signin!, only: [:show]
 before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -42,7 +43,7 @@ before_action :set_project, only: [:show, :edit, :update, :destroy]
 private
 
  def set_project
-   @project = Project.find(params[:id])
+   @project = Project.viewable_by(current_user).find(params[:id])
  rescue ActiveRecord::RecordNotFound
   flash[:alert] = "The project you were looking for could not be found."
   redirect_to projects_path
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,10 @@ class Project < ActiveRecord::Base
 	has_many :actions, dependent: :destroy
 	
   validates :name, presence: true
+
+  has_many :permissions, as: :thing
+
+  scope :viewable_by, ->(user) do
+  	joins(:permissions).where(permissions: {action: "view", user_id: user.id })
+  end
 end
