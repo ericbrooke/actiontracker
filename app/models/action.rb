f9513175059c8840b7c548825043a63380282d71
Eric Brooke <ericbrooke@mac.com>
2014-03-09 18:40:25 -0700

    setup action forms for mutiple assets upload

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,6 +1,8 @@
 class Action < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+  has_many :assets
+  accepts_nested_attributes_for :assets
 
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 10}
--- spec/features/creating_actions_spec.rb
+++ spec/features/creating_actions_spec.rb
@@ -61,4 +61,23 @@ feature "Creating Actions" do
 	  end
 	end
 
+	scenario "Creating a action with an attachment" do
+	  fill_in "Title", with: "Add documentation for blink tag"
+	  fill_in "Description", with: "The blink tag has a speed attribute"
+
+	  attach_file "File #1", Rails.root.join("spec/fixtures/speed.txt")
+	  attach_file "File #2", Rails.root.join("spec/fixtures/spin.txt")
+	  attach_file "File #3", Rails.root.join("spec/fixtures/gradient.txt")
+
+	  click_button "Create Action"
+
+	  expect(page).to have_content("Action has been created.")
+
+	  within("#action .assets") do
+	    expect(page).to have_content("speed.txt")
+	    expect(page).to have_content("spin.txt")
+	    expect(page).to have_content("gradient.txt")
+	  end
+	end
+    
 end
